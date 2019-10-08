34abe21d2c238fd47e6c305591aa6e6f2a3e509c
John Traut <jtraut1996@gmail.com>
2019-10-07 17:44:22 -0700

    almost

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -1,56 +1,74 @@
 var apiRouter = require("express").Router();
-var {friendData} = require("../data/friends");
+var { friendData } = require("../data/friends");
 //when going to /friends THE URL
 
 apiRouter.route("/friends")
-//send the object of friend data
-    .get(function(request, response){
-       response.send(friendData);
+    //send the object of friend data
+    .get(function (request, response) {
+        response.send(friendData);
 
     })//WHEN HANDLING POST REQUESTS TO THE URL
-    .post(function(request, response){
+
+    .post(function (request, response) {
+
+        var bestfriend = {
+            name: "",
+
+            absoluteScores:Infinity
+        };
 
         // response.json(request.body);
         console.log(friendData);
-        
+
         console.log("This is what is being taken in from the form", request.body)
         console.log("This is what that looks like to get acess to specific keys", request.body.name)
-      
+
 
         //This is where the data is being sent back out back to my html survey page
         //in this example im sending out exactly the same thing as what I got in
-        response.json(request.body);
-        
+        // response.json(request.body);
 
-        var friendDataArray = friendData[0].scores;
+
+        var friendDataArray = friendData;
         console.log("Wholefreinddata array", friendDataArray);
+
         var newFriendData = request.body
-        var newFreindTotal = 0; 
-        var talliedScored = 0;
+        console.log(newFriendData);
+     
 
-        for(let i=0; i< newFriendData.length; i++){
-            
-        }
-        
-        //loop through it
-        for(let i=0; i < friendData.length; i++){
-            for(let j=0; j <friendData[i].scores.length; j++){
-                // console.log(friendData[i].scores[j]);
-                if( [j] <= 9){
+        for (let i = 0; i < friendDataArray.length; i++) {
+            //grab the current friend
+            let currentFriend = friendDataArray[i]
 
-                    talliedScored += parseInt(friendData[i].scores[j])
-                } else {
+            var absolute = 0;
 
-                    return
-                }
-                
-                
-                
+            // iterate over the current friend's scores
+            for (let j = 0; j < currentFriend.length; j++) {
+
+                // tally up the absolute differencce between these two:
+                let currentFriendScore = currentFriend.scores[j];
+
+                let userScore = newFriendData.scores[j]
+
+               absolute += Math.abs(currentFriendScore - userScore);
             }
-            
-        }
+               //setting up an if statment tog grab new best friend
+               if(absolute < bestfriend.absoluteScores){
+
+                   bestfriend.name = newFriendData.name;
+
+                   bestfriend.absoluteScores = newFriendData.scores;
+
+                  
+               } else{
+                   
+               }
+
+            }
+            friendData.push(newFriendData);
+            response.send(bestfriend);
         
-         console.log(talliedScored);
+       
     })
 
 //export routes attached to apiRouter
